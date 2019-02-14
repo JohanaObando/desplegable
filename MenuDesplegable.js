a4bedd92f4119af66327bc09f7e70c92e74cc475
Johana <johanaobandob@gmail.com>
2019-02-13 20:21:57 -0600

    primer commit

---
+++ MenuDesplegable.js
@@ -0,0 +1,6 @@
+function ver (n){ /*PREGGUNTAR A JUAN PORQUE N PORQUE SUBSECCION Y NO SUBSECCION1*/
+document.getElementById("subseccion" + n).style.display= "block"
+}
+function ocultar (n){
+    document.getElementById("subseccion" + n).style.display= "none"
+}
